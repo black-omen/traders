09052b7f4380493a5f85797058ba686d8ad0192c
Samuel <sam.deslauriers@gmail.com>
2017-07-22 04:27:52 -0400

    Add minimal setup.py file
    This commit adds a minimal setup.py file which allows the package
    to be installed with `pip install -e ./`.

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='traders',
+    version='0.0.0',
+    description='A Python package used to build crypto currency traders',
+    author='Black Omen',
+    url='https://github.com/black-omen/traders',
+    packages=['traders'])
